a175d8cb75362169f21ad0fff22350f3f903634e
Dayanne Fernandes <dayannefernandesc@gmail.com>
2018-04-17 13:18:08 -0300

    add write headers in export csv

--- core/output.py
+++ core/output.py
@@ -2,28 +2,47 @@ import unittest
 import csv
 import os
 
+class dotdict(dict):
+    """dot.notation access to dictionary attributes"""
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
 class FileOutput:
 
-    # input_data has to be a list of dictionaries, e.g. [{}]
-    def __init__(self, input_data):
-        self.input_data = input_data
+    def __init__(self, filename=''):
+        self.filename = filename
+        self.result = dotdict({'status': False, 'error_msg': ''})
 
-    # Creates a csv file with the content of self.input_data
-    #   filename default value 'output.csv'
-    def export_CSV(self, filename = 'output.csv'):
+    # Creates a csv file with the content of input_data
+    #   input_data has to be a list of dictionaries, e.g. [{}]
+    #   If no header is passed, than the keys of the first value of input_data
+    #   list will become the headers
+    def export_CSV(self, input_data, headers=[]):
         try:
-            csv_file = open(filename, 'w')
-            writer = csv.DictWriter(csv_file, self.input_data[0].keys(),
-                                    quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
+            if not self.filename:
+                self.result.error_msg = 'ERROR: Nome do arquivo não informado'
+                return self.result
+
+            csv_file = open(self.filename, 'w')
+            if not headers:
+                headers = input_data[0].keys()
+
+            writer = csv.DictWriter(csv_file, fieldnames=headers,
+                                quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
+
             writer.writeheader()
-            for item in self.input_data:
+            for item in input_data:
                 writer.writerow((item))
             csv_file.close()
         except (FileNotFoundError, UnboundLocalError):
-            return 'ERROR: Nome do arquivo ou caminho incorreto'
-        except (AttributeError, IndexError):
+            self.result.error_msg = 'ERROR: Nome do arquivo ou caminho incorreto'
+            return self.result
+        except (AttributeError, IndexError, ValueError):
             csv_file.close()
-            os.remove(filename)
-            return 'ERROR: O tipo do dado está incorreto'
+            os.remove(self.filename)
+            self.result.error_msg = 'ERROR: O tipo do dado está incorreto'
+            return self.result
         else:
-            return os.path.isfile(filename);
+            self.result.status = os.path.isfile(self.filename);
+            return self.result
