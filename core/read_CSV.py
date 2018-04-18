e1197c757dcf7c4dc34c708f1bc58b94050996d1
joao96 <jvpoletti@gmail.com>
2018-04-18 16:29:00 -0300

    filling in youtube.csv file with data
    Co-authored-by: patrickvrb <patrickvrb@gmail.com>

---
+++ core/read_CSV.py
@@ -0,0 +1,49 @@
+from youtube.youtube import YoutubeAPI
+import csv
+
+class getInfo:
+
+    def __init__(self):
+        self.user = YoutubeAPI()
+
+    def read_actors_info(self):
+        with open('data/grupos_politicos.csv', 'r') as csvfile:
+            readCSV = csv.DictReader(csvfile)
+            actors_dict = []
+            for row in readCSV:
+                aux_dict = {'ator': '', 'username': '', 'id': ''}
+                if 'https' in row['YOUTUBE']:
+                    aux_dict['ator'] = row['FRENTES / COLETIVOS']
+                    if 'user' in row['YOUTUBE']:
+                        url = row['YOUTUBE'].split('/')
+                        index = url.index('user') + 1
+                        aux_dict['username'] = url[index]
+                        aux_dict['id'] = ''
+                    elif 'channel' in row['YOUTUBE']:
+                        url = row['YOUTUBE'].split('/')
+                        index = url.index('channel') + 1
+                        aux_dict['id'] = url[index]
+                        aux_dict['username'] = ''
+                    actors_dict.append(aux_dict)
+                elif 'S/' in row['YOUTUBE']:
+                    aux_dict['ator'] = row['FRENTES / COLETIVOS']
+                    aux_dict['id'] = ''
+                    aux_dict['username'] = ''
+                    actors_dict.append(aux_dict)
+        return actors_dict
+yt = getInfo()
+check = yt.user.generate_csv()
+if check:
+    actors_dict = yt.read_actors_info()
+    for item in actors_dict:
+        if item['username']:
+            result = yt.user.get_channel_info(item['username'])
+            item['id'] = result['items'][0]['id']
+        yt.user.insert_data(param = 'channel_id',
+                    value = item['id'],
+                    field_name = 'ator',
+                    field_value = item['ator'].replace('\n', ' '))
+        yt.user.insert_data(param = 'username',
+                    value = item['username'],
+                    field_name = 'ator',
+                    field_value = item['ator'].replace('\n', ' '))
