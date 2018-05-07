e1161861821eefc5099f078dc73ef3eea73561e5
Dayanne Fernandes <dayannefernandesc@gmail.com>
2018-05-07 03:52:20 -0300

    fix heroku test

--- tests/test_heroku.py
+++ tests/test_heroku.py
@@ -4,28 +4,21 @@ import json
 
 
 class TestHeroku(unittest.TestCase):
-    def test_home_status_code(self):
+
+    def test_list_actors(self):
         # Envia uma requisição HTTP GET para a aplicação
-        result = requests.get('https://youtube-data-monitor.herokuapp.com/')
+        result = requests.get(
+            'https://youtube-data-monitor.herokuapp.com/actors')
 
         # Verifica o código de estado da resposta da requisição
-        #   Se for sucesso irá retornar 200
         self.assertEqual(result.status_code, 200)
 
-        def test_list_actors(self):
-            # Envia uma requisição HTTP GET para a aplicação
-            result = requests.get(
-                'https://youtube-data-monitor.herokuapp.com/actors')
+        with open('data/actors.json') as data_file:
+            list_actors_original = json.load(data_file)
 
-            # Verifica o código de estado da resposta da requisição
-            self.assertEqual(result.status_code, 200)
+        r = json.loads(result.content.decode('utf8'))
 
-            with open('data/actors.json') as data_file:
-                list_actors_original = json.load(data_file)
-
-            r = json.loads(result.content.decode('utf8'))
-
-            self.assertEqual(r, list_actors_original)
+        self.assertEqual(r, list_actors_original)
 
 
 if __name__ == '__main__':
