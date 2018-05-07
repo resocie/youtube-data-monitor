8760f7529e31b402f854df9f6d7bf48905af7eeb
Dayanne Fernandes <dayannefernandesc@gmail.com>
2018-05-07 01:58:48 -0300

    add Procfile to heroku

--- tests/test_heroku.py
+++ tests/test_heroku.py
@@ -1,4 +1,3 @@
-from website.main import app
 import requests
 import unittest
 
@@ -6,7 +5,7 @@ import unittest
 class TestHeroku(unittest.TestCase):
     def test_home_status_code(self):
         # Envia uma requisição HTTP GET para a aplicação
-        result = resquests.get('https://youtube-data-monitor.herokuapp.com/')
+        result = requests.get('https://youtube-data-monitor.herokuapp.com/')
 
         # Verifica o código de estado da resposta da requisição
         #   Se for sucesso irá retornar 200
@@ -14,7 +13,7 @@ class TestHeroku(unittest.TestCase):
 
     def test_home_data(self):
         # Envia uma requisição HTTP GET para a aplicação
-        result = resquests.get('https://youtube-data-monitor.herokuapp.com/')
+        result = requests.get('https://youtube-data-monitor.herokuapp.com/')
 
         # Verifica o dado da resposta do caminho da página inicial
         self.assertEqual(result.data.decode("utf-8") , "Hello World.")
