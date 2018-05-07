4471afacba6f4a46c6e5ebaf9b6d912b71a9ea63
Dayanne Fernandes <dayannefernandesc@gmail.com>
2018-05-07 02:39:07 -0300

    add test api rest get method flask

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -1,6 +1,6 @@
 from website.main import app
 import unittest
-
+import json
 
 class TestFlask(unittest.TestCase):
     def setUp(self):
@@ -21,8 +21,20 @@ class TestFlask(unittest.TestCase):
         # Envia uma requisição HTTP GET para a aplicação
         result = self.app.get('/')
 
-        # Verifica o dado da resposta do caminho da página inicial
         self.assertEqual(result.data.decode("utf-8") , "Hello World.")
+
+    def test_get_actor_name_example(self):
+        # Envia uma requisição HTTP GET para a aplicação
+        result = self.app.get('/actor/example')
+
+        self.assertEqual(result.data.decode("utf-8") , "actor name : example")
+
+    def test_get_actor_name_json_example(self):
+        # Envia uma requisição HTTP GET para a aplicação
+        result = self.app.get('/actorjson/example')
+        r = json.loads(result.data.decode('utf8'))
+        
+        self.assertEqual(r , {'actor_name' : 'example'})
 
 if __name__ == '__main__':
     unittest.main()
--- website/main.py
+++ website/main.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
 import os
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello World."
+    return 'Hello World.'
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+@app.route('/actor/<actor_name>', methods=['GET'])
+def actor_name(actor_name):
+    return 'actor name : %s' % actor_name
+
+@app.route('/actorjson/<actor_name>', methods=['GET'])
+def actor_name_json(actor_name):
+    return jsonify({'actor_name' : actor_name})
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
