fb6cc8a235db0c82c8d011c20dbb63147f6224f4
Dayanne Fernandes <dayannefernandesc@gmail.com>
2018-05-07 02:04:12 -0300

    port to heroku

--- website/main.py
+++ website/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,5 @@ def hello():
     return "Hello World."
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
