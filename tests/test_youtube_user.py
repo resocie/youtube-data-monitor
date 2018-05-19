f9c82a86aa8ce6b5e09894bb348e23341ca8d2c4
Andre Garrido <andre.garridod@gmail.com>
2018-05-19 16:31:22 -0300

    Minor refactoring in tests
    Co-authored-by: joao96 <jvpoletti@gmail.com>

--- tests/test_youtube_user.py
+++ tests/test_youtube_user.py
@@ -60,7 +60,8 @@ class TestYoutubeAPI(unittest.TestCase):
         channel_id = 'UCsCI7wlAwbzTPK55yVaX2Ig'
         result = self._user.get_channel_info(channel_id)
         comment_count = self._user.get_channel_total_comment_count(result)
-        self.assertGreater(comment_count, '1')
+        self.assertGreaterEqual(comment_count, '0')
+        self.assertLessEqual(comment_count, '5')
 
     def test_get_channel_creation_date(self):
         channel_id = 'UC9uefWa6TXIPDRWGZYMcTuA'
