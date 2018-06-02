4f7e8f1a7948ee66b3ac6c8076b4851ac4bd4867
joao96 <jvpoletti@gmail.com>
2018-06-02 15:52:52 -0300

    videos PATH changed to database
    Co-authored-by: patrickvrb <patrickvrb@gmail.com>
    Co-authored-by: Adamacenog <andre.garridod@gmail.com>

--- server/main.py
+++ server/main.py
@@ -2,7 +2,7 @@ from core.output import FileOutput
 from server.api_exceptions import InvalidUsage
 from server.models import db
 from flask import Flask, jsonify
-from server.models import Actor
+from server.models import Actor, Videos
 import json
 import os
 
@@ -38,21 +38,19 @@ def list_dates():
 
 @app.route('/<date>/canal/<actor>/videos', methods=['GET'])
 def list_actor_videos_info(date, actor):
-    data_folders = [x[1] for x in os.walk('data/')][0]
 
     raise_date_error, raise_actor_error = True, True
-    list_actors_video_info = {'videos': []}
+    # list_actors_video_info = {'videos': []}
+    dates = db.session.query(Actor.collected_date).distinct()
+    all_dates = [item[0] for item in dates]
 
     if date == 'latest':
-        data_folders.sort()
-        folder = data_folders[-1]
-        date_file = 'data/'+folder+'/channel_videos/'
+        all_dates.sort()
+        given_date = all_dates[-1]
         raise_date_error = False
     else:
-        for folder in data_folders:
-            check_folder = folder.split('_')[0]
-            if date == check_folder:
-                date_file = 'data/'+folder+'/channel_videos/'
+        for item in all_dates:
+            if date == item:
                 raise_date_error = False
 
     if raise_date_error:
@@ -64,20 +62,25 @@ def list_actor_videos_info(date, actor):
                            status_code=450)
 
     actor = actor.replace('_', ' ')
-    data_folders = list(os.walk(date_file))[0][2]
-    for folder in data_folders:
-        if folder.replace('.csv', '').lower() == actor.lower():
-            actor_videos_info = FileOutput(date_file+folder).get_all_data()
-            list_actors_video_info['videos'] = actor_videos_info
-            raise_actor_error = False
+    info = db.session.query(Actor).filter_by(actor_name=actor).first()
+    if info is not None:
+        raise_actor_error = False
+        videos = db.session.query(Videos).\
+            filter_by(collected_date=date, channel_id=info.channel_id).all()
+        all_videos = []
+        for item in videos:
+            del item.__dict__['_sa_instance_state']
+            all_videos.append(item.__dict__)
 
     if raise_actor_error:
         raise InvalidUsage("Actor name was mistyped or this actor name don't"
-                           " exist in our database. Try list all the actors at"
+                           " exist in our database or"
+                           " there is no data to provide for this actor."
+                           " Try list all the actors at"
                            " youtube-data-monitor.herokuapp.com/actors.",
                            status_code=460)
 
-    return jsonify(list_actors_video_info)
+    return jsonify({'videos': all_videos})
 
 
 @app.route('/<date>/canal/<actor>', methods=['GET'])
